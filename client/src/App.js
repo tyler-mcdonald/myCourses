5faa6479cf9384beef22a67618bd7cc341a957a3
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-16 17:06:29 -0500

    Update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 /** Dependencies */
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 /** Components */
 import Head from "./Components/Head";
 import Header from "./Components/Header";
 import CourseDetail from "./Components/CourseDetail";
 import Courses from "./Components/Courses";
 import UpdateCourse from "./Components/UpdateCourse";
-import SignOut from "./Components/SignOut";
+// import SignOut from "./Components/SignOut";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
 import CreateCourse from "./Components/CreateCourse";
-
-// create CreateCourse
-// create UpdateCourse
-// create UseSignIn
-// create UserSignUp
-// create UserSignOut (stateless)
 
 function App() {
   return (
@@ -28,7 +29,9 @@ function App() {
           <Route exact path="/courses/create" element={<CreateCourse />} />
           <Route path="/courses/:id" element={<CourseDetail />} />
           <Route path="/courses/:id/update" element={<UpdateCourse />} />
-          <Route path="/signout" element={<SignOut />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="/signout" element={<Navigate replace to="/" />} />
         </Routes>
       </div>
       {/* </body> */}
