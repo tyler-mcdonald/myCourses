e907818a1c5e07a0a171a941386b429a0b57bad3
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-16 20:50:42 -0500

    Make CreateCourse component work

--- client/src/Components/CreateCourse.js
+++ client/src/Components/CreateCourse.js
@@ -1,15 +1,33 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import ValidationErrors from "./ValidationErrors";
 
 const CreateCourse = () => {
-  const createCourse = () => {
+  /** State */
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [estimatedTime, setEstimatedTime] = useState("");
+  const [materialsNeeded, setMaterialsNeeded] = useState("");
+  const [errors, setErrors] = useState([]);
+  /** Refs */
+  const titleInputRef = useRef("");
+  const descriptionInputRef = useRef("");
+  const estimatedTimeInputRef = useRef("");
+  const materialsNeededInputRef = useRef("");
+  /** Other */
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     axios
       .post(
         "http://localhost:5000/api/courses",
         {
-          title: "#TESTING",
-          description: "TESTING",
+          title,
+          description,
+          estimatedTime,
+          materialsNeeded,
           userId: 1,
         },
         {
@@ -19,24 +37,20 @@ const CreateCourse = () => {
           },
         }
       )
-      .then((response) => console.log(response));
+      .then((response) => {
+        console.log(response);
+        if (response.status === 201) {
+          return navigate("/");
+        }
+      })
+      .catch((err) => setErrors(() => err.response.data.errors));
   };
-
-  // useEffect(() => {
-  //   // createCourse();
-  // }, []);
 
   return (
     <main>
       <div className="wrap">
         <h2>Create Course</h2>
-        <div className="validation--errors">
-          <h3>Validation Errors</h3>
-          <ul>
-            <li>Please provide a value for "Title"</li>
-            <li>Please provide a value for "Description"</li>
-          </ul>
-        </div>
+        <ValidationErrors errors={errors} />
         <form>
           <div className="main--flex">
             <div>
@@ -45,15 +59,20 @@ const CreateCourse = () => {
                 id="courseTitle"
                 name="courseTitle"
                 type="text"
-                defaultValue=""
+                ref={titleInputRef}
+                onChange={() => setTitle(titleInputRef.current.value)}
               />
 
-              <p>By Joe Smith</p>
+              <p>By #user.name</p>
 
               <label htmlFor="courseDescription">Course Description</label>
               <textarea
                 id="courseDescription"
                 name="courseDescription"
+                ref={descriptionInputRef}
+                onChange={() =>
+                  setDescription(descriptionInputRef.current.value)
+                }
               ></textarea>
             </div>
             <div>
@@ -62,17 +81,27 @@ const CreateCourse = () => {
                 id="estimatedTime"
                 name="estimatedTime"
                 type="text"
-                defaultValue=""
+                ref={estimatedTimeInputRef}
+                onChange={() =>
+                  setEstimatedTime(estimatedTimeInputRef.current.value)
+                }
               />
 
               <label htmlFor="materialsNeeded">Materials Needed</label>
-              <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+              <textarea
+                id="materialsNeeded"
+                name="materialsNeeded"
+                ref={materialsNeededInputRef}
+                onChange={() =>
+                  setMaterialsNeeded(materialsNeededInputRef.current.value)
+                }
+              />
             </div>
           </div>
           <button
             className="button"
             type="submit"
-            onClick={() => createCourse()}
+            onClick={(e) => handleSubmit(e)}
           >
             Create Course
           </button>
