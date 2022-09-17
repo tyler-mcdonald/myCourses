a21936e9d35a5e7df74d3e1b974befdd01b97d22
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-17 18:46:22 -0500

    Create TextInputFile.js

---
+++ client/src/Components/TextInputField.js
@@ -0,0 +1,20 @@
+export const TextInputField = ({ dataValue, display, setUser }) => {
+  return (
+    <>
+      <label htmlFor={dataValue}>{display}</label>
+      <input
+        id={dataValue}
+        name={dataValue}
+        type="text"
+        onChange={(e) =>
+          setUser((prevState) => {
+            return {
+              ...prevState,
+              [dataValue]: e.target.value,
+            };
+          })
+        }
+      />
+    </>
+  );
+};
