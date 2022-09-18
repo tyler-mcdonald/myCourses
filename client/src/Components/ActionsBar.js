6cd16024c17b5a74937ea618bc45794caa361bbb
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-18 18:07:37 -0500

    Add dynamic display to Update/Delete buttons

--- client/src/Components/ActionsBar.js
+++ client/src/Components/ActionsBar.js
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../App";
 
 export const ActionsBar = ({ courseId, handleDelete }) => {
+  const user = useContext(UserContext);
+
   return (
     <div className="actions--bar">
       <div className="wrap">
-        <Link className="button" to={`/courses/${courseId}/update`}>
-          Update Course
-        </Link>
-        <Link className="button" onClick={() => handleDelete()}>
-          Delete Course
-        </Link>
+        {/* Display Update/Delete buttons if user is logged in */}
+        {user ? (
+          <>
+            <Link className="button" to={`/courses/${courseId}/update`}>
+              Update Course
+            </Link>
+            <Link className="button" onClick={() => handleDelete()}>
+              Delete Course
+            </Link>
+          </>
+        ) : null}
         <Link className="button button-secondary" to="/">
           Return to List
         </Link>
