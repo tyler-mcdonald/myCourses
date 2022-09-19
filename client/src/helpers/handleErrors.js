05e8f92d2f4b22732f756966db3cb5688630d2de
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-19 18:13:50 -0500

    Fix 403 redirect

--- client/src/helpers/handleErrors.js
+++ client/src/helpers/handleErrors.js
@@ -5,13 +5,14 @@ export const handleErrors = (error) => {
     handledError.messages = error.response.data.errors;
   }
   if (error.response.status === 403) {
+    handledError.route = "/forbidden";
     handledError.messages = error.response.data.message;
   }
   if (error.response.status === 404) {
     handledError.route = "/notfound";
   }
   if (error.response.status === 500) {
-    handledError.route = "/forbidden";
+    handledError.route = "/error";
   }
   return handledError;
 };
