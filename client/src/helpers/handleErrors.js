bfa725a6ff2f44b767b414654481a9a1720fb449
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-19 18:02:47 -0500

    Add error code

--- client/src/helpers/handleErrors.js
+++ client/src/helpers/handleErrors.js
@@ -1,14 +1,17 @@
 export const handleErrors = (error) => {
   const handledError = {};
   console.log(error);
-  if (error.response.status === 500) {
-    handledError.route = "/forbidden";
+  if (error.response.status === 400) {
+    handledError.messages = error.response.data.errors;
+  }
+  if (error.response.status === 403) {
+    handledError.messages = error.response.data.message;
   }
   if (error.response.status === 404) {
     handledError.route = "/notfound";
   }
-  if (error.response.status === 400) {
-    handledError.messages = error.response.data.errors;
+  if (error.response.status === 500) {
+    handledError.route = "/forbidden";
   }
   return handledError;
 };
