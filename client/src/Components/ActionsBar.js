f3b520bcef8d568f1497c59d6216711488969cc8
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-19 08:41:15 -0500

    Make buttons display if user is courseOwner

--- client/src/Components/ActionsBar.js
+++ client/src/Components/ActionsBar.js
@@ -2,14 +2,20 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../App";
 
-export const ActionsBar = ({ courseId, handleDelete }) => {
+export const ActionsBar = ({ courseId, handleDelete, courseOwner }) => {
   const user = useContext(UserContext);
+
+  const userIsCourseOwner = () => {
+    if (user && courseOwner) {
+      return user.emailAddress === courseOwner.emailAddress;
+    } else return false;
+  };
 
   return (
     <div className="actions--bar">
       <div className="wrap">
         {/* Display Update/Delete buttons if user is logged in */}
-        {user ? (
+        {user && userIsCourseOwner() ? (
           <>
             <Link className="button" to={`/courses/${courseId}/update`}>
               Update Course
