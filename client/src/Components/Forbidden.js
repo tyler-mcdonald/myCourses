8fb99826716a9fcafa6fe6a797a2cd8692651467
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-19 13:01:18 -0500

    Prevent direct access to "/courses/:id/update"

---
+++ client/src/Components/Forbidden.js
@@ -0,0 +1,3 @@
+export const Forbidden = () => {
+  return <h1>Forbidden</h1>;
+};
--- client/src/Components/UpdateCourse.js
+++ client/src/Components/UpdateCourse.js
@@ -1,20 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { UserContext } from "../App";
 import { ValidationErrors } from "./ValidationErrors";
-// import { Input } from "./Input";
-// import { TextArea } from "./TextArea";
-// import { SubmitButton } from "./SubmitButton";
-// import { CancelButton } from "./CancelButton";
 import { CourseInfoForm } from "./CourseInfoForm";
 
 const UpdateCourse = () => {
   const [course, setCourse] = useState({});
-  const [courseOwner, setCourseOwner] = useState({});
+  const [courseOwner, setCourseOwner] = useState();
   const [errors, setErrors] = useState([]);
+  const user = useContext(UserContext);
   const location = useLocation();
-  const courseId = location.pathname.split("/")[2];
+  const courseId = location.pathname.split("/")[2]; // use params instead
   const navigate = useNavigate();
+
+  // check if current loggeed in user is the course owner
+  const verifyCourseOwner = (owner) => {
+    if (owner) return user.emailAddress === owner.emailAddress;
+  };
+
+  const redirectIfForbidden = (result) => {
+    if (!result) return navigate("/forbidden");
+  };
 
   // get course data
   useEffect(() => {
@@ -23,8 +30,14 @@ const UpdateCourse = () => {
         .then((response) => response.json())
         .then((data) => {
           setCourse(data);
+          return data;
+        })
+        .then((data) => {
           setCourseOwner(data.User);
-        });
+          return data.User;
+        })
+        .then((owner) => verifyCourseOwner(owner))
+        .then((result) => redirectIfForbidden(result));
     }
     fetchData();
   }, [courseId]);
